295cb3f8fc19398767316e818dbea391302fbde9
ryan-byte <thesoulsreaper15@gmail.com>
2023-10-04 17:01:13 +0100

    Done with the website scraping script

--- getWebsiteData.js
+++ getWebsiteData.js
@@ -10,7 +10,7 @@ async function getWebsiteTrafficData(websiteDomain){
     const page = await browser.newPage();
 
     // Navigate to a website
-    console.log(`Accessing website (${websiteDomain}) traffic data`);
+    console.log(`Getting ${websiteDomain} traffic data`);
     await page.goto(`https://www.similarweb.com/website/${websiteDomain}`);
 
     // Wait for the page to fully loads
@@ -19,9 +19,31 @@ async function getWebsiteTrafficData(websiteDomain){
     await page.waitForLoadState('load');
 
     // Scrape traffic data from the page
+    console.log("Scraping data");
+    let data = await scrapeData(page);
 
     // Close the browser
     await browser.close();
+
+    // Returning scraped data
+    return data;
+}
+
+async function scrapeData(page){
+    const TrafficData = await page.evaluate(() => {
+        const totalVisitsParent = document.querySelector('[data-test="total-visits"]').parentElement;
+        const totalVisits = totalVisitsParent.querySelector('.engagement-list__item-value').textContent;
+
+        const pagePerVisitParent = document.querySelector('[data-test="pages-per-visit"]').parentElement;
+        const pagePerVisit = pagePerVisitParent.querySelector('.engagement-list__item-value').textContent;
+
+        const avgVisitDurationParent = document.querySelector('[data-test="avg-visit-duration"]').parentElement;
+        const avgVisitDuration = avgVisitDurationParent.querySelector('.engagement-list__item-value').textContent;
+
+        return {totalVisits, pagePerVisit, avgVisitDuration}
+    });
+
+    return TrafficData;
 }
 
 getWebsiteTrafficData("nend.io")
