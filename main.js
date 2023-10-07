9a89ff831c0016662db4deda139250dc77b0bc03
ryan-byte <thesoulsreaper15@gmail.com>
2023-10-07 10:07:54 +0100

    Done working on the scraping part

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const getWebsiteData = require("./lib/getWebsiteData.js");
-const {readJsonFile} = require("./lib/utils.js")
+const {readFile} = require("./lib/utils.js")
 
 async function main(){
-    
+    let websiteArr = await getWebsiteList(); 
+
+    // Scrape each website Data
+    for (let i = 0; i < websiteArr.length; i++){
+        let data = await getWebsiteData(websiteArr[i])
+        console.log(data);
+    }
+}
+
+async function getWebsiteList(){
+    let websiteFileData = await readFile("websites.txt");
+    return websiteFileData.split("\n")
 }
 
 
