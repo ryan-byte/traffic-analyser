ba2f562ffe6234c73f0f836c35cf75027ab13266
ryan-byte <thesoulsreaper15@gmail.com>
2023-10-07 10:59:03 +0100

    Now Scraped data are converted to excel sheet.

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 const getWebsiteData = require("./lib/getWebsiteData.js");
 const {readFile} = require("./lib/utils.js")
+const ConvertWebsitesDataToExcel = require("./lib/convertTrafficToExcel.js")
 
 async function main(){
     let websiteArr = await getWebsiteList(); 
+    console.log("\x1b[33m" + "Script launched." + "\x1b[0m \n");
 
     // Scrape each website Data
+    let scrapedData = []
     for (let i = 0; i < websiteArr.length; i++){
-        let data = await getWebsiteData(websiteArr[i])
-        console.log(data);
+        let websiteDomain = websiteArr[i];
+        let data = await getWebsiteData(websiteDomain);
+        let cleanData = {
+            websiteDomain,
+            data
+        };
+        
+        scrapedData.push(cleanData);
     }
+    console.log("\x1b[33m" + "Putting all scraped data into an Excel Sheet." + "\x1b[0m \n");
+
+    ConvertWebsitesDataToExcel(scrapedData);
 }
 
 async function getWebsiteList(){
